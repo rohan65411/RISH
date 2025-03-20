3c3b6b61ad52ec02e8251904ad9b2fe18c9a20b3
rishuheid <skfirdoush050@gmail.com>
2025-03-20 19:14:30 +0530

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -1,4 +1,4 @@
-.cmd install owner.js const { GoatWrapper } = require('fca-liane-utils');
+ const { GoatWrapper } = require('fca-liane-utils');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
