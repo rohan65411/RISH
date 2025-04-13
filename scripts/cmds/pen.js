edb522256a42c6ed9a938b507f14779e1c9b34d7
rishuheid <skfirdoush050@gmail.com>
2025-04-13 13:06:30 +0530

    Update pen.js

--- scripts/cmds/pen.js
+++ scripts/cmds/pen.js
@@ -48,7 +48,7 @@ module.exports = {
 
       try {
         await api.sendMessage(
-          `✅ Group has been Successfully Approved by Nazrul!\n\n📜 Type ${global.GoatBot.config.prefix}help to See Cmds!`,
+          `✅ Group has been Successfully Approved by RISHI!\n\n📜 Type ${global.GoatBot.config.prefix}help to See Cmds!`,
           group.threadID
         );
 
